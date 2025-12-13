35bacf539819017fffe8b8a5a11b1c82c48d656b
Wong Zhi Lin <s10267556@connect.np.edu.sg>
2025-12-13 23:33:34 +0800

    Working web portion of code + icons
    working sign in, sign up, log out, display user profile, and display leaderboards with working error messages as well
    
    css added
    
    comments added
    
    +icons used for leaderboard and profile

---
+++ firebaseConfig.js
@@ -0,0 +1,12 @@
+        // Your web app's Firebase configuration
+        export const firebaseConfig = {
+            apiKey: "AIzaSyAdgKvyD-Y9DNDLIHLB6LXOFprYIwETqrs",
+            authDomain: "dda-itd-asg.firebaseapp.com",
+            databaseURL: "https://dda-itd-asg-default-rtdb.asia-southeast1.firebasedatabase.app",
+            projectId: "dda-itd-asg",
+            storageBucket: "dda-itd-asg.firebasestorage.app",
+            messagingSenderId: "1099176217490",
+            appId: "1:1099176217490:web:e8097d3be6facf38decf4b"
+        };
+        
+        //exports firebaseConfig to be used in other files
